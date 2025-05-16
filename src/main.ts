7ffc724cb74e9d73bdebede2a5a67370958f2b28
조동현 <jofx55@gmail.com>
2025-05-16 14:20:27 +0900

    문제 범위 2~10으로로 제한

--- src/main.ts
+++ src/main.ts
@@ -60,10 +60,10 @@ function initializeGrid(forSolving: boolean = false, newNValue?: number) {
   } else {
     let inputValue =
       newNValue !== undefined ? newNValue : parseInt(nInput.value);
-    if (isNaN(inputValue) || inputValue <= 1 || inputValue > 30) {
+    if (isNaN(inputValue) || inputValue <= 1 || inputValue > 10) {
       // 최소/최대 크기 제한
       inputValue = 5;
-      alert("격자 크기는 2에서 30 사이로 설정해주세요.");
+      alert("격자 크기는 2에서 10 사이로 설정해주세요.");
     }
     n = inputValue;
     nInput.value = n.toString();
@@ -317,8 +317,8 @@ function handleActionButtonClick() {
 }
 function handleCreateRandomProblemClick() {
   const currentN = parseInt(nInput.value) || 5; // 현재 설정된 N값 사용 또는 기본값
-  if (isNaN(currentN) || currentN <= 1 || currentN > 30) {
-    alert("랜덤 문제 생성을 위한 격자 크기가 유효하지 않습니다. (2~30)");
+  if (isNaN(currentN) || currentN <= 1 || currentN > 10) {
+    alert("랜덤 문제 생성을 위한 격자 크기가 유효하지 않습니다. (2~10)");
     return;
   }
 
